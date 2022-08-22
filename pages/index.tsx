20dcd44402e3bacf82884fad0cc9553e1b524131
Gustavo Henz <59654620+GstavoHnz@users.noreply.github.com>
2022-08-22 10:07:37 -0300

    TE-107: Atualização Componentes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Filtro from './components/filtro'
-import Eventos from './components/eventos'
 
 const Home: NextPage = () => {
   return (
@@ -13,10 +11,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet"></link>
       </Head>
-      <h1>Todos os Eventos</h1>
-      <Filtro />
-      <Eventos />
-
+      <div className='container'>
+        <h1 className='text-center pt-8 '>Todos os Eventos</h1>
+      </div>
     </div>
   )
 }
