eba7caaedc3d2a1f0af65de93c98eefe6b5a3fdd
Gustavo Henz <59654620+GstavoHnz@users.noreply.github.com>
2022-08-26 10:10:16 -0300

    atualizacação

--- components/Social.tsx
+++ components/Social.tsx
@@ -4,10 +4,10 @@ const Social = () => {
   return (
     <div className='flex items-center md:items-center justify-end px-[7px]'>
       <ul className='grid grid-cols-4 gap-3 mb-[30px]'>
-        <a href=""><li className='bg-white w-[30px] h-[30px] rounded-full flex items-center justify-center text-[20px] hover:text-secundare-orange '>< FaLinkedinIn /></li></a>
-        <a href=""><li className='bg-white w-[30px] h-[30px] rounded-full flex items-center justify-center text-[20px] hover:text-secundare-orange '>< FaInstagram /></li></a>
-        <a href=""><li className='bg-white w-[30px] h-[30px] rounded-full flex items-center justify-center text-[20px] hover:text-secundare-orange '>< FaYoutube /></li></a>
-        <a href=""><li className='bg-white w-[30px] h-[30px] rounded-full flex items-center justify-center text-[20px] hover:text-secundare-orange '>< FaTwitter /></li></a>
+        <a href=""><li className='iconSocial bg-white w-[30px] h-[30px] rounded-full flex items-center justify-center text-[20px] hover:text-secundare-orange '>< FaLinkedinIn /></li></a>
+        <a href=""><li className='iconSocial bg-white w-[30px] h-[30px] rounded-full flex items-center justify-center text-[20px] hover:text-secundare-orange '>< FaInstagram /></li></a>
+        <a href=""><li className='iconSocial bg-white w-[30px] h-[30px] rounded-full flex items-center justify-center text-[20px] hover:text-secundare-orange '>< FaYoutube /></li></a>
+        <a href=""><li className='iconSocial bg-white w-[30px] h-[30px] rounded-full flex items-center justify-center text-[20px] hover:text-secundare-orange '>< FaTwitter /></li></a>
       </ul>
     </div>
   );
