f9951ed88b276c876d06f69884ad140f35c873e1
Gabriel Cano <gabriel@gabrielcano.com.br>
2022-09-01 21:02:08 -0300

    Dinamiza os filtros.

--- components/Filtros.tsx
+++ components/Filtros.tsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 
 {/* Importação de icons */ }
-import { FaPlus } from "react-icons/fa";
-import { FaMinus } from "react-icons/fa";
+import { FaPlus, FaMinus } from "react-icons/fa";
 
 interface Props {
   text: string,
-  list: string[]
+  list: any
 }
 
 const Filtros = ({ text, list }: Props) => {
@@ -20,12 +19,14 @@ const Filtros = ({ text, list }: Props) => {
       <p onClick={() => setAberto(!aberto)} className='grow text-[18px] p-[12px] leading-[22px] uppercase font-medium text-principal-black'>{text}</p>
 
       <ul style={{ display: aberto ? 'block' : 'none' }} className="my-2 py-4 w-full border-b-2 border-t-2">
-        {list.length > 0 && list.map((item, i) => (
-          <li className="py-1" key={i}>
+        {list.length > 0 && list.map((item : any) => (
+          <li className="py-1" key={item.id}>
             <label className="flex items-center relative">
-              <input type="checkbox" name={item}/>
+              <input type="checkbox" name={item.name} />
+
               <span className="border border-black absolute top-1 left-0 h-[15px] w-[15px] bg-white borde-radius-[2px]"></span>
-              <p className="ml-4 font-light">{item}</p>
+
+              <p className="ml-4 font-light">{item.name}</p>
             </label>
           </li>
         ))}
