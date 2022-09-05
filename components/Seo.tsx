b5ae1c81193fbcee2def61752f4fb3b4055e2014
Gabriel Cano <gcano@studiovisual.com.br>
2022-09-05 11:44:13 -0300

    Corrige favicon.

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -10,7 +10,7 @@ const Seo = ({ title, description }: Props) => {
     <Head>
       <title className='w-screen'>{title}</title>
       <meta name="description" content={description} />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.png" />
       <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet"></link>
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
